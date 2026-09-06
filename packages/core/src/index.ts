8eef2c549e16dd21e043019b41b9cc1cc259688f
Chris Brock <cbrock84@gmail.com>
2026-09-06 19:53:35 -0400

    feat(core): export core generation APIs

---
+++ packages/core/src/index.ts
@@ -0,0 +1,13 @@
+export { FakeModelProvider } from "./fake-provider.js";
+export { GenerationMachine } from "./generation-machine.js";
+export type {
+  GenerationPlan,
+  GenerationRequest,
+  GenerationResult,
+  GenerationState,
+  ModelProvider,
+  ProjectFile,
+  ProjectSnapshot,
+  ValidationResult,
+  Validator,
+} from "./types.js";
